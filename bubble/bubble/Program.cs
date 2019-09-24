629ed778ded19ed9bbca3d7e830f58db0af4f1e4
Kristina <55129343+lalkakris@users.noreply.github.com>
2019-09-24 12:01:40 +0300

    Add files via upload

---
+++ bubble/bubble/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace bubble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[20];
+            Console.WriteLine("Initial array:");
+            Reading(numbers);
+            Sorting(numbers);
+            Writing(numbers);
+         }
+        static void Reading(int[] numbers)
+        {
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(0, 100);
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+        static void Sorting(int[] numbers)
+        {
+            int temp;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+
+            }
+        }
+        static void Writing(int[] numbers)
+        {
+            Console.WriteLine("\nSorted array:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+
+    }
+}
